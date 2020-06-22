2cb2934d6ac6955f3aa47d95307e0e3d4aeb9e1b
Ultra <vit.lipin@gmail.com>
2020-06-22 13:08:12 +0300

    22.06 правки к консультации

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
     var button = document.querySelector(".searching");
     var loginpopup = document.querySelector(".hotel-search");
     var opened = false;
-    var form = document.querySelector("form");
+    var form = document.querySelector(".hotel-search");
+    var checker = loginpopup.querySelector(".dates-labels-one");
+    var checherTwo = loginpopup.querySelector(".dates");
 
     button.addEventListener("click", function(event) {
     if (!opened) {
@@ -13,4 +15,15 @@
         loginpopup.classList.remove("hotel-search-show");
         opened = false;
     }
+
+    form.addEventListener("submit", function (evt) {
+        if (!checker.value || dates.value) {
+            evt.preventDefault();
+            loginpopup.classList.add("modal-error");
+        } else {
+            if (isStorageSupport) {
+                localStorage.setItem("input", dates-labels-one.value)
+            }
+        }
+    })
 });
